ac92d2d5726c9b4d410c2ee36aadd2050d9e6014
carlwetzenstein <carlwetzenstein@gmail.com>
2013-10-29 15:56:40 +0100

    new test

--- app.js
+++ app.js
@@ -14,20 +14,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/bower", express.static(__dirname + "/bower_components"));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.get('/', routes.index);
 
-
 var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
