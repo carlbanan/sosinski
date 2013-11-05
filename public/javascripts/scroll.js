7e2c6f7562e6df654c8d521804d59f4e038f7af7
carlwetzenstein <carlwetzenstein@gmail.com>
2013-11-05 22:38:21 +0100

    js

--- public/javascripts/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function (){
-            $("#button").click(function (){
-                //$(this).animate(function(){
-                    $('html, body').animate({
-                        scrollTop: $(".second_section").offset().top
-                    }, 2000);
-                //});
-            });
-        });
